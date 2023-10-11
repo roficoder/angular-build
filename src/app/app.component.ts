a829cd349d1d324289951bc386edcf10fade9243
Abdul Raouf <raoufiscoder1033@gmail.com>
2023-10-12 00:26:21 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { EmailEditorComponent } from 'angular-email-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular-email-editor';
+
+  @ViewChild(EmailEditorComponent)
+  private emailEditor: EmailEditorComponent;
+
+  // called when the editor is created
+  editorLoaded(event) {
+    console.log('editorLoaded');
+    // load the design json here
+    // this.emailEditor.editor.loadDesign({});
+  }
+
+  // called when the editor has finished loading
+  editorReady(event) {
+    console.log('editorReady');
+  }
+
+  exportHtml() {
+    this.emailEditor.editor.exportHtml((data) =>
+      console.log('exportHtml', data)
+    );
+  }
 }
